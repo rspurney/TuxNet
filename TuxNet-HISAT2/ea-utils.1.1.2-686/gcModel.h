d34bda95a44a568dd8a49e77bfc0cc9371fe8535
Ryan Spurney <rjspurne@ncsu.edu>
2019-06-26 15:50:52 -0400

    TuxNet-HISAT2: gitignore ea-utils

--- TuxNet-HISAT2/ea-utils.1.1.2-686/gcModel.h
+++
@@ -1,7 +0,0 @@
-/*
-$Id: gcModel.h 556 2013-03-01 15:32:36Z earonesty $
-*/
-extern void gcInit(int maxReadLength);
-extern void gcProcessSequence(int l,int c);
-extern void gcPrintDistribution(FILE *fp);
-void gcClose();
--- TuxNet-HISAT2/ea-utils.1.1.2-686/utils.h
+++
@@ -1,5 +0,0 @@
-#include <string>
-#include <vector>
-
-std::string string_format(const std::string &fmt, ...);
-std::vector<char *> split(char* str, const char* delim);
