46c09e3b1582840cee378ca8f2a44501f3836f04
Ryan Spurney <rjspurne@ncsu.edu>
2019-07-08 14:15:17 -0400

    TuxNet-HISAT2: comment typos

--- TuxNet-HISAT2/GENIST/CheckInputsGENIST.m
+++ TuxNet-HISAT2/GENIST/CheckInputsGENIST.m
@@ -5,59 +5,59 @@ function [inputsStatus, clusteringFile, TFFile, symbolFile] = CheckInputsGENIST(
 %%
 inputsStatus = 1;
 
-if(strcmp(outputsFolder, '')) % Check if required filename has been provided
+if(strcmp(outputsFolder, ''))
     message = 'Please provide an outputs folder.';
     uialert(app.UIFigure, message, 'Error', 'Icon', 'error');
     app.GENIST_StatusField.Value = "Error";
     return;
 end
 
-if(strcmp(genesFile, '')) % Check if required filename has been provided
+if(strcmp(genesFile, ''))
     message = 'Please provide a genes file.';
     uialert(app.UIFigure, message, 'Error', 'Icon', 'error');
     app.GENIST_StatusField.Value = "Error";
     return;
 end
 
-if(strcmp(timecourseFile, '')) % Check if required filename has been provided
+if(strcmp(timecourseFile, ''))
     message = 'Please provide a timecourse file.';
     uialert(app.UIFigure, message, 'Error', 'Icon', 'error');
     app.GENIST_StatusField.Value = "Error";
     return;
 end
 
-if(exist(outputsFolder, 'dir') == 0) % Check if given file exists
+if(exist(outputsFolder, 'dir') == 0)
     message = 'Provided outputs folder cannot be found.';
     uialert(app.UIFigure, message, 'Error', 'Icon', 'error');
     app.GENIST_StatusField.Value = "Error";
     return;
 end
 
-if(exist(genesFile, 'file') == 0) % Check if given file exists
+if(exist(genesFile, 'file') == 0)
     message = 'Provided genes file cannot be found.';
     uialert(app.UIFigure, message, 'Error', 'Icon', 'error');
     app.GENIST_StatusField.Value = "Error";
     return;
 end
 
-if(exist(timecourseFile, 'file') == 0) % Check if given file exists
+if(exist(timecourseFile, 'file') == 0)
     message = 'Provided timecourse file cannot be found.';
     uialert(app.UIFigure, message, 'Error', 'Icon', 'error');
     app.GENIST_StatusField.Value = "Error";
     return;
 end
 
-if(strcmp(filenameResults, '')) % Check if output filename has been provided
+if(strcmp(filenameResults, ''))
     message = 'Please provide an output file name.';
     uialert(app.UIFigure, message, 'Error', 'Icon', 'error');
     app.GENIST_StatusField.Value = "Error";
     return;
 end
 
-if(strcmp(clusteringFile, '')) % Check if optional file is given
+if(strcmp(clusteringFile, ''))
     clusteringFile = [];
 else
-    if(exist(clusteringFile, 'file') == 0) % Check if optional file exists
+    if(exist(clusteringFile, 'file') == 0)
         message = 'Provided clustering file cannot be found.';
         uialert(app.UIFigure, message, 'Error', 'Icon', 'error');
         app.GENIST_StatusField.Value = "Error";
@@ -65,10 +65,10 @@ else
     end
 end
 
-if(strcmp(TFFile, '')) % Check if optional file is given
+if(strcmp(TFFile, ''))
     TFFile = [];
 else
-    if(exist(TFFile, 'file') == 0) % Check if optional file exists
+    if(exist(TFFile, 'file') == 0)
         message = 'Provided TF file cannot be found.';
         uialert(app.UIFigure, message, 'Error', 'Icon', 'error');
         app.GENIST_StatusField.Value = "Error";
@@ -76,10 +76,10 @@ else
     end
 end
 
-if(strcmp(symbolFile, '')) % Check if optional file is given
+if(strcmp(symbolFile, ''))
     symbolFile = [];
 else
-    if(exist(symbolFile, 'file') == 0) % Check if optional file exists
+    if(exist(symbolFile, 'file') == 0)
         message = 'Provided gene name file cannot be found.';
         uialert(app.UIFigure, message, 'Error', 'Icon', 'error');
         app.GENIST_StatusField.Value = "Error";
